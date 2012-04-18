d0b8527fe44be01d1388d51c49b7037c1bf28013
Andrew Carter <andrew.r.carter@gmail.com>
2012-04-17 23:35:05 -0400

    Initial Commit

---
+++ OGLKit/OGLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OGLAppDelegate.h
+//  OGLKit
+//
+//  Created by Andrew Carter on 4/17/12.
+//  Copyright (c) 2012 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OGLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OGLKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OGLKit
+//
+//  Created by Andrew Carter on 4/17/12.
+//  Copyright (c) 2012 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OGLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OGLAppDelegate class]));
+    }
+}
