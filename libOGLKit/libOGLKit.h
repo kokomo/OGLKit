49f6aeaa3411d7c9e9e16a95ed76a53e321a11e9
Andrew Carter <andrew.r.carter@gmail.com>
2012-04-18 00:39:49 -0400

    now builds lib

---
+++ libOGLKit/libOGLKit.h
@@ -0,0 +1,13 @@
+//
+//  libOGLKit.h
+//  libOGLKit
+//
+//  Created by Andrew Carter on 4/18/12.
+//  Copyright (c) 2012 WillowTree Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libOGLKit : NSObject
+
+@end
---
+++ libOGLKit/libOGLKit.m
@@ -0,0 +1,13 @@
+//
+//  libOGLKit.m
+//  libOGLKit
+//
+//  Created by Andrew Carter on 4/18/12.
+//  Copyright (c) 2012 WillowTree Apps. All rights reserved.
+//
+
+#import "libOGLKit.h"
+
+@implementation libOGLKit
+
+@end
