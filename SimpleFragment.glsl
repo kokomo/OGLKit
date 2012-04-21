4596e44530e325f3a651fb9f983a1e25e95b3f38
Andrew Carter <andrew.r.carter@gmail.com>
2012-04-20 23:30:51 -0400

    shader adjustments

--- SimpleFragment.glsl
+++ SimpleFragment.glsl
@@ -16,14 +16,14 @@ float diff = max(0.0, dot(normalize(v_Normal), normalize(lightVector)));
 float diffuse = max(dot(v_Normal, lightVector), 0.25) * 0.2;
 
 gl_FragColor = diff * (diffuse * v_Color);
-gl_FragColor += (v_Color * 0.75);
+gl_FragColor += (v_Color * 0.15);
 
 vec3 vReflection = normalize(reflect(-normalize(lightVector),normalize(v_Normal)));
  
  float spec = max(0.0, dot(normalize(v_Normal), vReflection));
 
  if (diff != 0.0) {
-   float fSpec = pow(spec, 32.0);
+   float fSpec = pow(spec, 256.0);
    gl_FragColor.rgb += vec3(fSpec, fSpec, fSpec);
  }
 
